4a157134db3a8aed7731fde788ce1ad492d17589
Matt Brown <jellyphishbrain@gmail.com>
2017-07-27 20:21:46 -0500

    Update iife.js

--- iife.js
+++ iife.js
@@ -11,7 +11,7 @@ function showDogFood(dogFood) {
 	for (let i = 0; i < dogBrands[0].types.length; i++) {
 
 		dogBrandDiv.innerHTML += `<h2>${dogBrands[i].name}</h2>
-								  <hr>`;
+					  <hr>`;
 		console.log(dogBrands[i]);
 
 		for (let j = 0; j < dogBrands[i].types.length; j++) {
@@ -21,8 +21,8 @@ function showDogFood(dogFood) {
 			for (let k = 0; k < dogBrands[i].types[j].volumes.length; k++) {
 
 				dogBrandDiv.innerHTML += `<h4>${dogBrands[i].types[j].type}</h4>
-										  <h5>${dogBrands[i].types[j].volumes[k].name}</h5>
-					 					  <h5>${dogBrands[i].types[j].volumes[k].price}</h5>`;	
+							  <h5>${dogBrands[i].types[j].volumes[k].name}</h5>
+					 		  <h5>${dogBrands[i].types[j].volumes[k].price}</h5>`;	
 				console.log(dogBrands[i].types[j].volumes[k]);
 			}
 		}		
@@ -36,8 +36,8 @@ function showCatFood(catFood) {
 	for (let i = 0; i < catBrands.length; i++) {
 
 		catBrandDiv.innerHTML += `<h2>${catBrands[i].name}</h2>
-								  <hr>
-								  <h3>Breeds</h3>`;
+					  <hr>
+					  <h3>Breeds</h3>`;
 		console.log(catBrands[i]);
 
 		for (let j = 0; j < catBrands[i].breeds.length; j++) {
@@ -48,8 +48,8 @@ function showCatFood(catFood) {
 
 		for (let k = 0; k < catBrands[i].volumes.length; k++) {
 			catBrandDiv.innerHTML += `<h4>${catBrands[i].volumes[k].type}</h4>
-									  <h5>${catBrands[i].volumes[k].amount}</h5>
-									  <h5>${catBrands[i].volumes[k].price}</h5>`;
+						  <h5>${catBrands[i].volumes[k].amount}</h5>
+						  <h5>${catBrands[i].volumes[k].price}</h5>`;
 			console.log(catBrands[i].volumes[k]);
 		}
 
